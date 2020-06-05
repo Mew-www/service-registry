51fad128ed685495e2b622d8580436d9bb0d9f1d
Mew-www <mew.proxy@hotmail.com>
2020-06-06 02:17:37 +0300

    Fix permissions (imtoosleepy z.z)

--- user_api/permissions.py
+++ user_api/permissions.py
@@ -5,3 +5,8 @@ class IsThisUserOrAdministrator(permissions.BasePermission):
     # Object-permission where object is THE user themselves, or AN admin
     def has_object_permission(self, request, view, obj):
         return obj == request.user or request.user.is_staff
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
